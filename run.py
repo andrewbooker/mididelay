351f131b71fdad9db664f4bee4fc8c2a4a66e2ea
Andrew Booker <improvizone@gmail.com>
2020-04-11 18:03:27 +0100

    basic note repetition with decreasing velocity

--- run.py
+++ run.py
@@ -49,18 +49,42 @@ class MidiIo():
 		del self.player
 		del self.receiver
 
+def repeater(player, playedNote, v):
+	note = playedNote
+	player.note_on(note, v, 0)
+	time.sleep(0.1)
+	player.note_off(note, 0, 0)
+
+	dv = v / 10.0
+	velocity = v - dv
+
+	while velocity > 0:
+		time.sleep(1.9)
+		player.note_on(note, int(velocity), 0)
+		time.sleep(0.1)
+		player.note_off(note, 0, 0)
+		velocity -= dv
+
+
 devs = UsingMidiDevices()
 io = MidiIo(devs.forInput(), devs.forOutput())
 
 
+import threading
 received = 0
-while received < 10:
+while received < 100:
 	if io.receiver.poll():
 		e = io.receiver.read(1)
-		io.player.write(e)
-		print(e)
-		received += 1
+		msg = e[0][0]
+		note = msg[1]
+		vel = msg[2]
+		if vel > 0:
+			t = threading.Thread(target = repeater, args = (io.player, note, vel), daemon = True)
+			t.start()
+			received += 1
+			print("latest", msg, "of", received)
 
+time.sleep(20)
 
 del io
 del devs
