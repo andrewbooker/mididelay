bb8c3b7e992f341fc16a007f90c5134cec141b53
Andrew Booker <improvizone@gmail.com>
2020-04-28 23:51:21 +0100

    switching to using mediautils for midi device lookup

---
+++ README.md
@@ -0,0 +1,7 @@
+# mididelay
+
+requires mediautils project in adjacent direcory:
+
+git clone https://github.com/andrewbooker/mediautils.git
+pip install pygame (for mediautils)
+
--- run.py
+++ run.py
@@ -1,55 +1,9 @@
 #!/usr/bin/env python
 
-import pygame.midi as midi
 import time
 import keyboard
 import random
 
-
-class UsingMidiDevices():
-	def __init__(self):
-		print("listing usable devices")
-		midi.init()
-		self.outputs = []
-		self.inputs = []
-		count = midi.get_count()
-		print("found", count, "devices")
-		for d in range(count):
-			info = midi.get_device_info(d)
-			name = info[1].decode("utf-8")
-			
-			isOutput = (info[3] == 1)
-			isInput = (info[2] == 1)
-			if isOutput and "USB" in name: self.outputs.append((d, name))
-			if isInput and "USB" in name: self.inputs.append((d, name))
-				
-		print("inputs:", self.inputs)
-		print("outputs:", self.outputs)
-		if len(self.inputs) == 0 or len(self.outputs) == 0:
-			print("need both input and output USB devices")
-			exit()
-		
-	def forInput(self):
-		return self.inputs[0][0] if len(self.inputs) > 0 else None
-
-	def forOutput(self):
-		return self.outputs[0][0] if len(self.outputs) > 0 else None
-
-	def __del__(self):
-		midi.quit()
-		print("exiting")
-
-
-class MidiIo():
-	def __init__(self, forInput, forOutput):
-		self.receiver = midi.Input(forInput)
-		self.player = midi.Output(forOutput, latency = 0)
-
-	def __del__(self):
-		self.player.close()
-		self.receiver.close()
-		del self.player
-		del self.receiver
 
 from multiprocessing import Value
 import threading
@@ -101,8 +55,13 @@ class Repeater():
 
 		del(self)
 
-devs = UsingMidiDevices()
-io = MidiIo(devs.forInput(), devs.forOutput())
+import sys
+import os
+sys.path.append(os.path.dirname(os.getcwd()))
+from mediautils.usbdevices import UsbMidiDevices, MidiIn, MidiOut
+devs = UsbMidiDevices()
+ioOut = MidiOut(devs)
+ioIn = MidiIn(devs)
 
 shouldStop = threading.Event()
 def stop(e):
@@ -135,8 +94,8 @@ key = Key()
 
 
 while not shouldStop.is_set():
-	if io.receiver.poll():
-		e = io.receiver.read(1)
+	if ioIn.io.poll():
+		e = ioIn.io.read(1)
 		msg = e[0][0]
 		note = msg[1]
 		vel = msg[2]
@@ -144,10 +103,11 @@ while not shouldStop.is_set():
 			if note == 8:
 				key.transpose()
 			else:
-				Repeater(io.player, note, vel, key.scale)
+				Repeater(ioOut.io, note, vel, key.scale)
 
 for r in Repeater.repeaters[:]:
 	del(r)
 
-del io
+del ioOut
+del ioIn
 del devs
